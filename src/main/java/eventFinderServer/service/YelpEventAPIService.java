bb48ea086d6c6feea2354ccbb3e9a528543b0750
yuehua <huang.yueh@husky.neu.edu>
2019-04-17 15:54:51 -0400

    test

--- src/main/java/eventFinderServer/service/YelpEventAPIService.java
+++ src/main/java/eventFinderServer/service/YelpEventAPIService.java
@@ -77,6 +77,7 @@ public class YelpEventAPIService {
   }
   
   
+  /*
   
   @GetMapping ("/api/events/paged")
   public Page<Event> findPagedEvents(@RequestParam(name="page", required=false) Integer page,
@@ -93,7 +94,7 @@ public class YelpEventAPIService {
       return pagedRepository.findAll(p);
 
 	  
-  }
+  }*/
   
   
   /*
